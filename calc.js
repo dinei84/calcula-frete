b3d3613da87849ea15f4eb1e20ae209e63b3b547
dinei84 <dinei.pita@gmail.com>
2024-02-23 10:00:48 -0300

    atl

--- calc.js
+++ calc.js
@@ -1,31 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("valor").addEventListener("keyup", function(event) {
-        if (event.key === "Enter") {
-            clicar();
-        }
-    });
-
-    document.getElementById("pedagio").addEventListener("keyup", function(event) {
-        if (event.key === "Enter") {
-            clicar();
-        }
-    });
-
-    document.getElementById("peso").addEventListener("keyup", function(event) {
-        if (event.key === "Enter") {
-            clicar();
-        }
-    });
-
-    document.getElementById("botao").addEventListener("click", function() {
+// Adiciona um ouvinte de evento para a tecla "Enter" nos campos de entrada
+document.getElementById("valor").addEventListener("keyup", function(event) {
+    // Verifica se a tecla pressionada é a tecla "Enter"
+    if (event.key === "Enter") {
+        // Chama a função clicar() quando a tecla "Enter" for pressionada
         clicar();
-    });
-
-    document.getElementById("clean").addEventListener("click", function() {
-        limparCampos();
-    });
+    }
 });
 
+document.getElementById("pedagio").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        clicar();
+    }
+});
+
+document.getElementById("peso").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        clicar();
+    }
+});
+
+// Função para calcular
 function clicar() {
     var valor = parseFloat(document.getElementById('valor').value);
     var pedagio = parseFloat(document.getElementById('pedagio').value);
@@ -38,17 +32,8 @@ function clicar() {
     var vsdsa = res * peso;
     var ajt2 = vsdsa / 1000;
     var ajt3 = ajt2 * 0.7;
-    var ajt4 = ajt3 * (1 - 0.018);
-    var adianta = ajt4.toFixed(2);
+    var adianta = ajt3.toFixed(2);
 
     document.getElementById('res').value = res.toFixed(2);
     document.getElementById('adianta').value = adianta;
-}
-
-function limparCampos() {
-    document.getElementById('valor').value = "";
-    document.getElementById('pedagio').value = "";
-    document.getElementById('peso').value = "";
-    document.getElementById('res').value = "";
-    document.getElementById('adianta').value = "";
 }
